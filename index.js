29adab2af6423cf784b7e4d26e1ab59eb7491c98
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-05-19 20:51:51 -0400

    completed challenge with complexity 12

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const largestSubarraySum = (array) => {
     let n = array.length
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    const sumArray = []
+    const isAbove = (value) => value >= 0
+    const isBelow = (value) => value < 0
+     
 
-    debugger
+    if(array.every(isAbove)){
+        sumArray.push(array.reduce(reducer))
+    }else if(array.every(isBelow)){
+        sumArray.push(0);
+    }else {
     for(let i = 0; i < n; i++){
         for(let j = i; j < n; j++){
+            const array2 = []
             for(let k = i; k <= j; k++){
-                console.log(array[k])
+                array2.push(array[k])
             }
+            sumArray.push(array2.reduce(reducer))
         }
+        
     }
+}
+     return Math.max.apply(null, sumArray)
 }
